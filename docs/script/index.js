04775d61df8d0c009445bb74feb38b0974ff3b1f
Hidetaro7 <hidetaro7@gmail.com>
2021-02-25 23:40:44 +0900

    fixed: iPadの崩れなど修正

--- docs/script/index.js
+++ docs/script/index.js
@@ -1,1 +1,13 @@
 console.log('Hello ニューロナWorld');
+
+const bodyElement = document.body;
+
+function onScroll () {
+  if(window.scrollY >= 70) {
+    bodyElement.classList.add("is-headerfixed")
+  }else{
+    bodyElement.classList.remove("is-headerfixed")
+  }
+}
+
+window.onscroll = onScroll;
