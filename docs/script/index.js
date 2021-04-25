5e1a767a6ce11bee0ddb67dcfc6061caba370171
Hidetaro7 <hidetaro7@gmail.com>
2021-04-25 11:51:55 +0900

    feat: Youtube動画とSwiper.jsの実装

--- docs/script/index.js
+++ docs/script/index.js
@@ -11,3 +11,40 @@ function onScroll () {
 }
 
 window.onscroll = onScroll;
+
+// swiper
+
+var mySwiper = new Swiper ('.swiper-container', {
+  breakpoints: {
+    320: {
+      slidesPerView: 1.5,
+    },
+    768: {
+      slidesPerView: 2,
+    },
+    1024: {
+      slidesPerView: 2,
+    },
+    1280: {
+      slidesPerView: 3,
+    }
+  },
+  spaceBetween: 16,//何ピクセル画像の間隔をあけるか
+  centeredSlides : true,
+  //自動再生する場合
+  autoplay: {
+  delay: 2000,
+  },
+  loop: true,//最後の画像までいったらループする
+  //ページネーションをつける場合
+  pagination: {
+   el: '.swiper-pagination',
+   type: 'bullets',
+   clickable: true,
+  },
+  //左右のナビゲーションをつける場合
+  navigation: {
+   nextEl: '.swiper-button-next',
+   prevEl: '.swiper-button-prev',
+  }
+ });
