833ca5860428348b21bc86bb73877468045e77e0
Hideki Akiba <hidetaro7@gmail.com>
2021-08-11 19:24:53 +0900

    Update src/_data/articles.js
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- src/_data/articles.js
+++ src/_data/articles.js
@@ -7,7 +7,7 @@ module.exports = async () => {
   try {
     const res = await axios.get('https://tuqulore.microcms.io/api/v1/neurona', {
       headers: {
-        "X-API-KEY": process.env.XKEY,
+        'X-API-KEY': process.env.XKEY,
       },
     });
     // eslint-disable-next-line no-console
