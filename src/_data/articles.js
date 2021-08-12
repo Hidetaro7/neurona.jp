d5c5f38cb2fe611ed66139244f117302d9b49987
Hidetaro7 <Hidetaro7@users.noreply.github.com>
2021-08-12 01:03:53 +0000

    bot: format

--- src/_data/articles.js
+++ src/_data/articles.js
@@ -1,13 +1,13 @@
 // eslint-disable-next-line import/no-extraneous-dependencies
-const { default: axios } = require('axios');
-require('dotenv').config();
+const { default: axios } = require("axios");
+require("dotenv").config();
 
 // eslint-disable-next-line consistent-return
 module.exports = async () => {
   try {
-    const res = await axios.get('https://tuqulore.microcms.io/api/v1/neurona', {
+    const res = await axios.get("https://tuqulore.microcms.io/api/v1/neurona", {
       headers: {
-        'X-API-KEY': process.env.XKEY,
+        "X-API-KEY": process.env.XKEY,
       },
     });
     // eslint-disable-next-line no-console
