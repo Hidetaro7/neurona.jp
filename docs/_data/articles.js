25282664c1fd5e9ddc470b469c951e5f7413b708
Hidetaro7 <hidetaro7@gmail.com>
2021-10-04 23:34:26 +0900

    feat: ギャラリーを追加

--- docs/_data/articles.js
+++ docs/_data/articles.js
@@ -1,5 +1,7 @@
 // eslint-disable-next-line import/no-extraneous-dependencies
 const { default: axios } = require("axios");
+const HTMLParser = require("node-html-parser");
+
 require("dotenv").config();
 
 // eslint-disable-next-line consistent-return
@@ -11,10 +13,52 @@ module.exports = async () => {
       },
     });
     // eslint-disable-next-line no-console
-    console.log(res.data);
-    return res.data;
+    // console.log(res.data);
+    const renderHTML = parsePhohoGallery(res.data);
+    console.log(renderHTML);
+    console.log("--------------------");
+    return renderHTML;
   } catch (error) {
     // eslint-disable-next-line no-console
     console.error(error);
   }
+};
+
+const parsePhohGallery2 = (htmldata) => {
+  const resArray = { contents: [] };
+  htmldata.contents.forEach((article) => {
+    if (article.photoGallery) {
+      const newData = { ...article };
+      const galleryRoot = HTMLParser.parse(article.photoGallery);
+      galleryRoot.querySelectorAll("img").forEach((img) => {
+        //console.log(img);
+        newData.photoGallery += `<a href="${img.src}"><img src="${img.src}" alt="" /></a>`;
+      });
+      resArray.contents.push(newData);
+    } else {
+      resArray.contents.push(article);
+    }
+    //console.log(resArray.contents);
+  });
+};
+
+const parsePhohoGallery = (htmldata) => {
+  const resArray = { ...htmldata };
+  //console.log(resArray);
+  resArray.contents.forEach((article) => {
+    if (article.photoGallery) {
+      let images = "";
+      HTMLParser.parse(article.photoGallery)
+        .querySelector("p")
+        .querySelectorAll("img")
+        .forEach((img) => {
+          const attr = img.attributes;
+          images += `<a href="${attr.src}"><img src="${attr.src}" alt="" width="${attr.width}" height="${attr.height}" /></a>`;
+        });
+      article.photoGallery = images;
+    }
+    /* console.log(resArray);
+    console.log("--------------------"); */
+  });
+  return resArray;
 };
