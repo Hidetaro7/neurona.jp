e82bebefb344611d55a247fd27641dfcc93ce87e
Hidetaro7 <hidetaro7@gmail.com>
2022-05-06 15:01:35 +0000

    Update!

--- docs/script/index.js
+++ docs/script/index.js
@@ -2,18 +2,17 @@ const bodyElement = document.body;
 
 function onScroll() {
   if (window.scrollY >= 70) {
-    bodyElement.classList.add('is-headerfixed');
+    bodyElement.classList.add("is-headerfixed");
   } else {
-    bodyElement.classList.remove('is-headerfixed');
+    bodyElement.classList.remove("is-headerfixed");
   }
 }
 
 window.onscroll = onScroll;
 
-/*global Swiper,IframeLightbox*/
-/*eslint no-undef: "error"*/
+// swiper
 
-new Swiper('.swiper-container', {
+const mySwiper = new Swiper(".swiper-container", {
   breakpoints: {
     320: {
       slidesPerView: 1.5,
@@ -37,27 +36,27 @@ new Swiper('.swiper-container', {
   loop: true, // 最後の画像までいったらループする
   // ページネーションをつける場合
   pagination: {
-    el: '.swiper-pagination',
-    type: 'bullets',
+    el: ".swiper-pagination",
+    type: "bullets",
     clickable: true,
   },
   // 左右のナビゲーションをつける場合
   navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
   },
 });
 
-const menuButton = document.querySelector('.btn-trigger');
-const drawerMenu = document.querySelector('.drawer-menu');
-menuButton.addEventListener('click', () => {
-  menuButton.classList.toggle('active');
-  drawerMenu.classList.toggle('active');
+const menuButton = document.querySelector(".btn-trigger");
+const drawerMenu = document.querySelector(".drawer-menu");
+menuButton.addEventListener("click", () => {
+  menuButton.classList.toggle("active");
+  drawerMenu.classList.toggle("active");
 });
 
 [].forEach.call(
-  document.getElementsByClassName('iframe-lightbox-link'),
+  document.getElementsByClassName("iframe-lightbox-link"),
   (el) => {
     el.lightbox = new IframeLightbox(el);
-  },
+  }
 );
--- docs/script/lightbox.js
+++ docs/script/lightbox.js
@@ -8,26 +8,26 @@
  * passes jshint
  */
 (function (root, document) {
-  const addEventListener = 'addEventListener';
-  const getElementById = 'getElementById';
-  const getElementsByClassName = 'getElementsByClassName';
-  const createElement = 'createElement';
-  const classList = 'classList';
-  const appendChild = 'appendChild';
-  const dataset = 'dataset';
-  const containerClass = 'iframe-lightbox';
-  const isLoadedClass = 'is-loaded';
-  const isOpenedClass = 'is-opened';
-  const isShowingClass = 'is-showing';
+  const addEventListener = "addEventListener";
+  const getElementById = "getElementById";
+  const getElementsByClassName = "getElementsByClassName";
+  const createElement = "createElement";
+  const classList = "classList";
+  const appendChild = "appendChild";
+  const dataset = "dataset";
+  const containerClass = "iframe-lightbox";
+  const isLoadedClass = "is-loaded";
+  const isOpenedClass = "is-opened";
+  const isShowingClass = "is-showing";
   const IframeLightbox = function (elem, settings) {
     const options = settings || {};
     this.trigger = elem;
     this.rate = options.rate || 500;
-    this.el = document[getElementsByClassName](containerClass)[0] || '';
-    this.body = this.el ? this.el[getElementsByClassName]('body')[0] : '';
-    this.content = this.el ? this.el[getElementsByClassName]('content')[0] : '';
-    this.href = elem[dataset].src || '';
-    this.paddingBottom = elem[dataset].paddingBottom || '';
+    this.el = document[getElementsByClassName](containerClass)[0] || "";
+    this.body = this.el ? this.el[getElementsByClassName]("body")[0] : "";
+    this.content = this.el ? this.el[getElementsByClassName]("content")[0] : "";
+    this.href = elem[dataset].src || "";
+    this.paddingBottom = elem[dataset].paddingBottom || "";
     // Event handlers
     this.onOpened = options.onOpened;
     this.onIframeLoaded = options.onIframeLoaded;
@@ -42,8 +42,10 @@
       this.create();
     }
     const debounce = function (func, wait) {
-      let timeout; let args; let context; let
-        timestamp;
+      let timeout;
+      let args;
+      let context;
+      let timestamp;
       return function () {
         context = this;
         args = [].slice.call(arguments, 0);
@@ -68,28 +70,28 @@
     };
     const debounceHandleOpenIframeLightbox = debounce(
       handleOpenIframeLightbox,
-      this.rate,
+      this.rate
     );
-    this.trigger[addEventListener]('click', debounceHandleOpenIframeLightbox);
+    this.trigger[addEventListener]("click", debounceHandleOpenIframeLightbox);
   };
   IframeLightbox.prototype.create = function () {
     const _this = this;
-    const bd = document[createElement]('div');
-    this.el = document[createElement]('div');
-    this.content = document[createElement]('div');
-    this.body = document[createElement]('div');
+    const bd = document[createElement]("div");
+    this.el = document[createElement]("div");
+    this.content = document[createElement]("div");
+    this.body = document[createElement]("div");
     this.el[classList].add(containerClass);
-    bd[classList].add('backdrop');
-    this.content[classList].add('content');
-    this.body[classList].add('body');
+    bd[classList].add("backdrop");
+    this.content[classList].add("content");
+    this.body[classList].add("body");
     this.el[appendChild](bd);
     this.content[appendChild](this.body);
-    this.contentHolder = document[createElement]('div');
-    this.contentHolder[classList].add('content-holder');
+    this.contentHolder = document[createElement]("div");
+    this.contentHolder[classList].add("content-holder");
     this.contentHolder[appendChild](this.content);
     this.el[appendChild](this.contentHolder);
     document.body[appendChild](this.el);
-    bd[addEventListener]('click', () => {
+    bd[addEventListener]("click", () => {
       _this.close();
     });
     const clearBody = function () {
@@ -97,24 +99,18 @@
         return;
       }
       _this.el[classList].remove(isShowingClass);
-      _this.body.innerHTML = '';
+      _this.body.innerHTML = "";
     };
-    this.el[addEventListener]('transitionend', clearBody, false);
-    this.el[addEventListener]('webkitTransitionEnd', clearBody, false);
-    this.el[addEventListener]('mozTransitionEnd', clearBody, false);
-    this.el[addEventListener]('msTransitionEnd', clearBody, false);
+    this.el[addEventListener]("transitionend", clearBody, false);
+    this.el[addEventListener]("webkitTransitionEnd", clearBody, false);
+    this.el[addEventListener]("mozTransitionEnd", clearBody, false);
+    this.el[addEventListener]("msTransitionEnd", clearBody, false);
     this.callCallback(this.onCreated, this);
   };
   IframeLightbox.prototype.loadIframe = function () {
     const _this = this;
     this.iframeId = containerClass + Date.now();
-    this.body.innerHTML = `<iframe src="${
-      this.href
-    }" name="${
-      this.iframeId
-    }" id="${
-      this.iframeId
-    }" onload="this.style.opacity=1;" style="opacity:0;border:none;" scrolling="no" webkitallowfullscreen="true" mozallowfullscreen="true" allowfullscreen="true" height="166" frameborder="no" allow="autoplay"></iframe>`;
+    this.body.innerHTML = `<iframe src="${this.href}" name="${this.iframeId}" id="${this.iframeId}" onload="this.style.opacity=1;" style="opacity:0;border:none;" scrolling="no" webkitallowfullscreen="true" mozallowfullscreen="true" allowfullscreen="true" height="166" frameborder="no" allow="autoplay"></iframe>`;
     (function (iframeId, body) {
       document[getElementById](iframeId).onload = function () {
         this.style.opacity = 1;
@@ -122,14 +118,14 @@
         _this.callCallback(_this.onIframeLoaded, _this);
         _this.callCallback(_this.onLoaded, _this);
       };
-    }(this.iframeId, this.body));
+    })(this.iframeId, this.body);
   };
   IframeLightbox.prototype.open = function () {
     this.loadIframe();
     if (this.paddingBottom) {
       this.content.style.paddingBottom = this.paddingBottom;
     } else {
-      this.content.removeAttribute('style');
+      this.content.removeAttribute("style");
     }
     this.el[classList].add(isShowingClass);
     this.el[classList].add(isOpenedClass);
@@ -144,11 +140,11 @@
     return this.el[classList].contains(isOpenedClass);
   };
   IframeLightbox.prototype.callCallback = function (func, data) {
-    if (typeof func !== 'function') {
+    if (typeof func !== "function") {
       return;
     }
     const caller = func.bind(this);
     caller(data);
   };
   root.IframeLightbox = IframeLightbox;
-}(typeof window !== 'undefined' ? window : this, document));
+})(typeof window !== "undefined" ? window : this, document);
