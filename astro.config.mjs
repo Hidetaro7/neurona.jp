fb8624119254e0213049fff77ac5ac0b430ef7a0
Hidetaro7 <hidetaro7@gmail.com>
2026-01-27 21:28:41 +0900

    feat: Tailwind v4を導入。

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,25 +1,22 @@
-import { defineConfig } from 'astro/config';
-import tailwind from '@astrojs/tailwind';
-import sitemap from '@astrojs/sitemap';
-import mdx from '@astrojs/mdx';
+import { defineConfig } from "astro/config";
+import tailwindcss from "@tailwindcss/vite";
+import sitemap from "@astrojs/sitemap";
+import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://neurona.jp',
-  output: 'static',
-  srcDir: './src',
-  publicDir: './src/public',
-  integrations: [
-    tailwind({
-      applyBaseStyles: false,
-    }),
-    sitemap(),
-    mdx(),
-  ],
-  outDir: './dist',
+  site: "https://neurona.jp",
+  output: "static",
+  srcDir: "./src",
+  publicDir: "./src/public",
+  integrations: [sitemap(), mdx()],
+  outDir: "./dist",
   image: {
     // Configure image optimization
-    formats: ['webp'],
+    formats: ["webp"],
     quality: 80,
+  },
+  vite: {
+    plugins: [tailwindcss()],
   },
 });
--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,8 +1,3 @@
-import postcss from 'postcss';
-import postcssImport from 'postcss-import';
-import postcssMixins from 'postcss-mixins';
-import tailwindcssNesting from 'tailwindcss/nesting';
-import tailwindcss from 'tailwindcss';
 import autoprefixer from 'autoprefixer';
 import cssnano from 'cssnano';
 
@@ -10,17 +5,6 @@ export default ({ env }) => ({
   map: false,
   from: undefined,
   plugins: [
-    {
-      postcssPlugin: 'grouped',
-      Once(root, { result }) {
-        return postcss([
-          postcssImport({ root: 'src/style' }),
-          postcssMixins,
-        ]).process(root, result.opts);
-      },
-    },
-    tailwindcssNesting,
-    tailwindcss,
     autoprefixer,
     env === 'production' ? cssnano : false,
   ],
