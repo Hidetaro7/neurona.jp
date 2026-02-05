abc9709d89c80750b966c4cb011abb8656a5a6c9
Hidetaro7 <hidetaro7@gmail.com>
2026-02-05 10:07:36 +0900

    feat: SEO文章の見直しと改善

--- src/lib/site.ts
+++ src/lib/site.ts
@@ -1,6 +1,7 @@
 export const site = {
-  name: 'ニューロナ',
-  description: 'ニューロナの公式ページ',
-  origin: 'https://neurona.jp',
-  ogpImage: '/image/ogp_image.png',
+  name: "ニューロナ",
+  description:
+    "ニューロナはアンダーグラウンドなポップ＆ロックバンド。猫のしもべの人間たちが集まって音楽活動しています。主に神奈川県湘南地区を拠点にライブ活動を展開中。",
+  origin: "https://neurona.jp",
+  ogpImage: "/image/ogp_image.png",
 };
